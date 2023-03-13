c081fabb57394531542d32578ea48e1c45d8448b
Javen Jin <Javen.Jin@grapecity.com>
2023-03-13 14:07:11 +0800

    Modify the project to React-TS project

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from './components/Weather';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Javen Jin
+				</p>
+				<Weather />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
