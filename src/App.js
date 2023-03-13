3f61a3fb64c49538a19baf29e6a4be8292bc6d84
Javen Jin <Javen.Jin@grapecity.com>
2023-03-13 12:26:10 +0800

    Revert "Remove Default React App"
    This reverts commit d4ca058f83c2af8aa52b3679f846ef655a564a18.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import logo from './logo.svg';
 import './App.css';
 import Weather from './components/Weather';
 
 function App() {
     return (
         <div className="App">
-            <p>
-                Javen Jin
-            </p>
-            <Weather />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Javen Jin
+                </p>
+                <Weather />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
         </div>
     );
 }
