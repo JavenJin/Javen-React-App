b681ab02f9137e46c0053d36209211deee505507
Javen Jin <Javen.Jin@grapecity.com>
2023-03-13 14:47:11 +0800

    Automatically get the current time Xi'an weather

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Build a React app on GitHub (2023-03-11)
 
 Build tutorial: [Getting Started with GitHub Actions Tutorial](https://www.ruanyifeng.com/blog/2019/09/getting-started-with-github-actions.html)
+
+## React-ts and add automatically get the current time Xi'an weather (2023-03-13)
+
+Get current weather: [wttr.in](https://github.com/chubin/wttr.in)
--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 
+interface WeatherState {
+    weather: string;
+}
+
 export default class Weather extends React.PureComponent {
 
-    getWeather = () => {
-        return 'Xian: ☀️   🌡️+16°C 🌬️→27km/h';
+    state: WeatherState = {
+        weather: 'Loading...',
+    }
+
+    City: string = 'Xian';
+
+    async componentDidMount(): Promise<void> {
+        this.getWeather();
+    }
+
+    getWeather = async () => {
+        const weather = await fetch(`https://wttr.in/${this.City}?format=4`).then(response => response.text());
+        this.setState({ weather });
     }
 
     render() {
         return (
-            <div>{this.getWeather()}</div>
+            <div>{this.state.weather}</div>
         )
     }
 }
